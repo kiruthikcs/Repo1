fcb965628796c4a85498f271a0287dd2ad0d1780
mannavar <mouna.annavarapu@capgemini.com>
2017-06-05 17:34:47 +0530

    Add files via upload

---
+++ class1.java
@@ -0,0 +1,16 @@
+
+public class class1 {
+	
+	void method1() {
+	    System.out.println("This is class1");
+	  }
+	 
+	  public static void main(String[] args) {
+		  class1 class1Obj = new class1();
+	    class2 class2Obj = new class2();
+	 
+	    class1Obj.method1();
+	    class2Obj.method2();
+	  }
+
+}
