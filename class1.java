7b4d0be61063957f876b0a7a03636e7671fffb43
mannavar <mouna.annavarapu@capgemini.com>
2017-06-05 17:39:02 +0530

    Update class1.java

--- class1.java
+++ class1.java
@@ -2,7 +2,7 @@
 public class class1 {
 	
 	void method1() {
-	    System.out.println("This is class1");
+	    System.out.println("This is class1. This can be edited!");
 	  }
 	 
 	  public static void main(String[] args) {
