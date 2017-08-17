5b5f5dc97e82c49bc3dae086376ac6b98ae73bc4
kiruthikcs <kiruthika.a.k@capgemini.com>
2017-08-17 14:05:03 +0530

    commit to check rebase ....
    Rebase trial

--- class1.java
+++ class1.java
@@ -6,10 +6,21 @@ public class class1 {
 	  }
 	 
 	  public static void main(String[] args) {
+		  
+		  
+		  
 		  class1 class1Obj = new class1();
+		  
+		  
+		  
 	    class2 class2Obj = new class2();
+		  
+		  
 	 
 	    class1Obj.method1();
+		  
+		  
+		  
 	    class2Obj.method2();
 	  }
 
